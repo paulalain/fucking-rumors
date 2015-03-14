e9bba84a996ccf5e30993caa6f99fac69ca93d20
Paul-Alain Bugnard <PaulAlain@Armaggeddon.local>
2015-03-14 16:19:13 +0100

    Add somes files

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      scripts: {
+        files: ['src/lib/**/*', 'src/client/**/*'],
+        tasks: ['uglify']
+      },
+      style: {
+        files: 'stylesheets/**/*',
+        tasks: ['concat', 'sass', 'cssmin']
+      },
+      assets: {
+        files: 'assets/**/*',
+        tasks: ['copy']
+      }
+    },
+
+    concat: {
+      vendor_style: {
+        src: [
+          'node_modules/bootstrap/dist/css/bootstrap.min.css',
+          'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
+        dest: 'public/style/bootstrap.css'
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'public/style/app.css': 'style/app.scss',
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'public/scripts/app.min.js': 'public/scripts/app.js'
+        }
+      }
+    },
+
+    cssmin: {
+      dist: {
+        files: {
+          'public/style/app.min.css': 'public/style/app.css',
+          'public/style/bootstrap.min.css': 'public/style/bootstrap.css',
+        }
+      }
+    },
+
+    copy: {
+      assets: {
+        expand: true,
+        cwd: 'assets/',
+        src: '**',
+        dest: 'public/assets/'
+      },
+      fonts: {
+        expand: true,
+        cwd: 'node_modules/font-awesome/',
+        src: 'fonts/**',
+        dest: 'public/assets/'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', [
+    'concat', 'uglify', 'sass', 'cssmin', 'copy'
+  ]);
+  grunt.registerTask('heroku', [
+    'concat', 'uglify', 'sass', 'cssmin', 'copy'
+  ]);
+};
